3ce708b677bb1237808070a42211463166039e4e
Christopher Black <chris@primeacademy.io>
2017-10-27 11:47:27 -0500

    Bling scope example.

--- client.js
+++ client.js
@@ -1,12 +1,3 @@
-# $scope -> controller as
-
-Converting `$scope` to `controller as` syntax.
-
-## $scope
-
-When using `$scope`, it must be passed into the controller function. Anything attached to `$scope` is available in the HTML for your `ng-controller`. 
-
-```JavaScript
 var myApp = angular.module('myApp', []);
 
 myApp.controller('GreetController', function($scope){
@@ -17,4 +8,3 @@ myApp.controller('GreetController', function($scope){
         $scope.message = 'Hello ' + personToGreet + '!';
     }
 });
-```
